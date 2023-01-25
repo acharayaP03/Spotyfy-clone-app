12275433ab67dd9fe781c1508e5aa2d78e30fcae
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-26 09:07:13 +1100

    build query to get details of songs when user clicks on songs

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -6,13 +6,14 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', '');
+      headers.set('X-RapidAPI-Key', '2406d572c4msh3b45e698894d40fp17782ejsn51cb7954b272');
 
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}`})
   }),
 });
 
