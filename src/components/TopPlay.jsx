ce0b535111b3b75a1a4ee765ae2321eb581a937b
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-25 22:58:23 +1100

    top artist secions

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -10,6 +10,16 @@ import { FreeMode } from "swiper";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { handlePlayPause, handlePauseClick } from "../utilities";
+
+import  'swiper/css';
+import 'swiper/css/free-mode';
+
+const TopChartCard = ({ song, i}) => (
+    <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-6 p-4 rounded-lg cursor-pointer mg-2">
+        {song.title}
+    </div>
+)
 
 const TopPlay = () => {
     const dispatch = useDispatch();
@@ -24,17 +34,48 @@ const TopPlay = () => {
         divRef.current.scrollIntoView({behavior: 'smooth'});
     })
 
-    const handlePauseClick = () =>{
-        dispatch(playPause(false))
-    }
-
-    const handlePlayPause = () =>{
-        dispatch(setActiveSong({ song, data, i }))
-        dispatch(playPause(true))
-    }
+    /**
+     * only display 5 songs
+     * */
+    const topPlays = data?.slice(0, 5);
+    // const handlePauseClick = () =>{
+    //     dispatch(playPause(false))
+    // }
+    //
+    // const handlePlayPause = () =>{
+    //     dispatch(setActiveSong({ song, data, i }))
+    //     dispatch(playPause(true))
+    // }
     return (
-        <div ref={divRef}>
-
+        <div
+            ref={divRef}
+            className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col"
+        >
+            <div className="w-full flex flex-col">
+                <div className="flex flex-row justify-between items-center">
+                    <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+                    <Link to="/top-charts">
+                        <p className="text-gray-300 text-base cursor-pointer">See more</p>
+                    </Link>
+                </div>
+                <div className="mt-4 flex flex-col gap-1">
+                    { topPlays?.map(( song, i) => (
+                        <TopChartCard
+                            key={song.key}
+                            song={song}
+                            i={i}
+                        />
+                    ))}
+                </div>
+                <div className="w-full flex flex-col mt-8">
+                    <div className="flex flex-row justify-between items-center">
+                        <h2 className="text-white font-bold text-2xl">Top Artists</h2>
+                        <Link to="/top-atists">
+                            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
