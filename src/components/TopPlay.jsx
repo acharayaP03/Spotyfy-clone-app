92019de335da0ec96da4ce4f5cc8aeff9307cd7f
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-25 22:09:49 +1100

    initial setup for top play page

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,23 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+/**
+ * Swiper is for user swipe on images of artists section
+ */
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+const TopPlay = () => {
+
+    return (
+        <>
+            top charts
+        </>
+    )
+}
 
 export default TopPlay;
