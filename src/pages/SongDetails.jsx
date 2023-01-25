aa13df7aa4ba0bdfa6bf9ac0f82cb11bb113aa87
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-26 09:00:50 +1100

    initial song details set up with state and router params

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,16 @@
-const SongDetails = () => <div>SongDetails</div>;
+import  { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+
+const SongDetails = () => {
+    const dispatch = useDispatch();
+    const { songid } = useParams();
+    const { activeSong, isPlaying } = useSelector((state) => state.player)
+
+    console.log('Song Id : ', songid)
+    return( <div>song details</div>)
+}
 
 export default SongDetails;
