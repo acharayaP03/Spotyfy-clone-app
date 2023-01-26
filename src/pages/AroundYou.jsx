313026fecaf35faca3a823df88bad3aca8978ffa
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-26 19:35:01 +1100

    base component setup with state

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error,Loader, SongCard } from '../components';
+
+/**
+ * Realtime ip location api will be used to identify the top tracks thats happening in that country.
+ *
+ * @url geo.ipify.com
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const CountryTracks = () => {
+    const [country, setCountry ] = useState('');
+    const [loading, setLoading ] = useState(true);
+    const { activeSong, isPlaying } = useSelector((state) => state.player)
+    return (
+        <div className="text-white">
+            Around You..
+        </div>
+    )
+};
 
 export default CountryTracks;
