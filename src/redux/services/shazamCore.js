e446d167f101b9a45278d5d91d13f8f37d3de2e1
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-26 17:39:11 +1100

    add new query builder to get artists detial

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    baseUrl: 'https://shazam-core.p.rapidapi.com',
     prepareHeaders: (headers) => {
       headers.set('X-RapidAPI-Key', '');
 
@@ -12,14 +12,16 @@ export const shazamCoreApi = createApi({
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => '/charts/world' }),
-    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}`}),
-    getRelatedSongs: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}`})
+    getTopCharts: builder.query({ query: () => '/v1/charts/world' }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/v1/tracks/details?track_id=${songid}`}),
+    getRelatedSongs: builder.query({ query: ({ songid }) => `/v1/tracks/related?track_id=${songid}`}),
+    getArtistDetails: builder.query({query: (artistId) => `/v2/artists/details?artist_id=${artistId}`})
   }),
 });
 
 export const {
   useGetTopChartsQuery,
   useGetSongDetailsQuery,
-  useGetRelatedSongsQuery
+  useGetRelatedSongsQuery,
+    useGetArtistDetailsQuery
 } = shazamCoreApi;
