97f076fb9d4bfe94b0990b1f9eb5bbb470aba843
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-26 20:02:17 +1100

    request location with geo ipify org

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 
 import { Error,Loader, SongCard } from '../components';
+import {logo} from "../assets";
 
 /**
  * Realtime ip location api will be used to identify the top tracks thats happening in that country.
@@ -15,6 +16,21 @@ const CountryTracks = () => {
     const [country, setCountry ] = useState('');
     const [loading, setLoading ] = useState(true);
     const { activeSong, isPlaying } = useSelector((state) => state.player)
+
+
+    /**
+     * Get location from here..
+     */
+
+    useEffect(() =>{
+
+        axios.get(`${import.meta.env.VITE_GEO_API_URL}`,{
+            params: { apiKey: import.meta.env.VITE_GEO_API_KEY}
+        }).then( result => console.log(result?.data?.location?.country))
+            .catch((error) => console.log(error))
+            .finally(() => setLoading(false))
+
+    }, [country])
     return (
         <div className="text-white">
             Around You..
